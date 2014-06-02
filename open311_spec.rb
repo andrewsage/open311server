f0cc034fb71f1d96e4e15b079412e6bebb8ec433
Andrew Sage <andrew@asae.co.uk>
2014-06-02 11:48:31 +0100

    Test added to ensure that all the required fields for a Service within a Services request are present.

--- open311_spec.rb
+++ open311_spec.rb
@@ -39,6 +39,19 @@ describe 'The Open311 App' do
       xml_doc  = Nokogiri::XML(last_response.body)
       expect(xml_doc.xpath('services')).not_to be_empty
     end
+    
+    it "each service should contain the required fields" do
+      required_fields = %w(service_code service_name description metadata type keywords group)
+      
+      get '/dev/v2/services.xml'
+      xml_doc  = Nokogiri::XML(last_response.body)
+      xml_doc.xpath("//service").each do |service_xml|
+        required_fields.each do |field|
+          #puts "#{field} = #{service_xml.xpath(field).text}"
+          expect(service_xml.xpath(field)).not_to be_empty
+        end
+      end
+    end
   end
   
   describe 'get service' do
