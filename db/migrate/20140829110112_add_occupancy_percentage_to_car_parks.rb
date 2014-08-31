5485f157c8f286b87db61009dd4566a5c7b9de13
Andrew Sage <andrew@asae.co.uk>
2014-08-31 21:39:15 +0100

    car park occupancy_percentage decimal format changed

--- db/migrate/20140829110112_add_occupancy_percentage_to_car_parks.rb
+++ db/migrate/20140829110112_add_occupancy_percentage_to_car_parks.rb
@@ -1,5 +1,5 @@
 class AddOccupancyPercentageToCarParks < ActiveRecord::Migration
   def change
-    add_column :car_parks, :occupancy_percentage, :decimal
+    add_column :car_parks, :occupancy_percentage, :decimal, :decimal, :precision => 16, :scale => 3
   end
 end
