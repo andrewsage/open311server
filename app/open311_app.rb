7fdb77b3597f43e283a330f003913123f209c336
Andrew Sage <andrew@asae.co.uk>
2014-09-04 17:10:12 +0100

    logging added

--- app/open311_app.rb
+++ app/open311_app.rb
@@ -2,10 +2,26 @@ require 'json'
 require 'mechanize'
 require "sinatra/activerecord"
 require 'sinatra/contrib'
+require 'logger'
+
 
 class Open311App < Sinatra::Base
   register Sinatra::ActiveRecordExtension
   register Sinatra::Contrib
+
+  ::Logger.class_eval { alias :write :'<<' }
+  access_log = ::File.join(::File.dirname(::File.expand_path(__FILE__)),'log','access.log')
+  access_logger = ::Logger.new(access_log)
+  error_logger = ::File.new(::File.join(::File.dirname(::File.expand_path(__FILE__)),'log','error.log'),"a+")
+  error_logger.sync = true
+
+  configure do
+    use ::Rack::CommonLogger, access_logger
+  end
+
+  before {
+    env["rack.errors"] =  error_logger
+  }
 
   set :database_file, '../config/database.yml'
   set :services_api_root, '/dev/v2'
